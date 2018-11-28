7d0da96b243b6ddec6124a1373e447d47cd55ce6
yurii.puk <yurii.puk@eleks.com>
2018-11-28 15:05:04 +0200

    Binary Serealization of Object is Faster than JSON

---
+++ BinarySerealization/BinarySerealization/Program.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BinarySerealization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            byte[] deviceAsType = null;
+            byte[] deviceAsJson = null;
+
+            DeviceCharacteristics device = new DeviceCharacteristics
+            {
+                Humidity = 10,
+                Temperature = 10
+            };
+
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bf.Serialize(ms, device);
+                deviceAsType = ms.ToArray();
+
+                bf.Serialize(ms, JsonConvert.SerializeObject(device));
+                deviceAsJson = ms.ToArray();
+            }
+
+            Console.WriteLine("JSON - {0}", deviceAsJson.Length);
+            Console.WriteLine("Type - {0}", deviceAsType.Length);
+            Console.ReadLine();
+
+        }
+    }
+
+
+    [Serializable]
+    class DeviceCharacteristics
+    {
+        public double Temperature { get; set; }
+
+        public double Humidity { get; set; }
+    }
+}

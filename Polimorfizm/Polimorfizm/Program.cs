42417248dac0aae7f2bf18470c43acf971f3fb3f
yurii.puk <yurii.puk@eleks.com>
2019-02-13 11:32:15 +0200

    master - polimorfizm tricky moments

---
+++ Polimorfizm/Polimorfizm/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Polimorfizm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass baseClassc = new BaseClass();
+            InheritClass inheritClass = new InheritClass();
+
+            BaseClass test = (BaseClass)inheritClass;
+            test.VirtualMethod(); // Hello from inherit
+            //test.VirtualMethod(); // Hello from base
+
+
+            Console.ReadLine();
+        }
+    }
+
+    class BaseClass
+    {
+        public virtual void VirtualMethod()
+        {
+            Console.WriteLine("Hello from base");
+        }
+    }
+
+    class InheritClass : BaseClass
+    {
+        public override void VirtualMethod()
+        {
+            Console.WriteLine("Hello from inherit");
+        }
+
+        //public new void VirtualMethod()
+        //{
+        //    Console.WriteLine("Hello from inherit");
+        //}
+    }
+}
